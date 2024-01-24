12cf1a3e16f8389611fa07055ef55a8afbac746b
druzhee <mohannadsa54@gmail.com>
2024-01-24 15:28:25 +0100

    test

--- Very Interesting API and App/Controllers/WeatherForecastController.cs
+++ Very Interesting API and App/Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace Very_Interesting_API_and_App.Controllers
 			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
 				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-				TemperatureC = Random.Shared.Next(-20, 55),
+				TemperatureC = -20,
 				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
 			})
 			.ToArray();
